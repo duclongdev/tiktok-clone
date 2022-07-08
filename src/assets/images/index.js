9154ec2deb80cd73f552b4910a27390f98a44816
duclongdev <20521566@gm.uit.edu.vn>
2022-07-08 22:23:29 +0700

    complete search component

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+  logo: require('../images/logo.svg').default,
+  close: require('../images/close.svg').default,
+  search: require('../images/search.svg').default,
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,25 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cs = classNames.bind(styles);
 function Header() {
   return (
     <header className={cs('wrapper')}>
-      <div className={cs('inner')}></div>
+      <div className={cs('inner')}>
+        <img src={images.logo} alt="TikTok" />
+        <div className={cs('search-container')}>
+          <input type="text" placeholder="Search account and videos" />
+          {/* <img src={images.close} alt="close" /> */}
+          {/* <FontAwesomeIcon className={cs('loading-icon')} icon={faSpinner} /> */}
+          <button className={cs('btn-search')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
